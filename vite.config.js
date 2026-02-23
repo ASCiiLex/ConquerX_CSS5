6a599887d8d52f775aebfc39bc53c59148e31b29
ASCiiLex <alejandroscuetara@gmail.com>
2026-02-23 19:26:32 +0100

    Ajustes pages + vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,38 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  build: {
-    outDir: 'docs'
-  },
+  base: "/ConquerX_CSS5/",
+
   root: ".",
+
+  build: {
+    outDir: "docs",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+
+        blog: resolve(__dirname, "pages/blog.html"),
+        blog1: resolve(__dirname, "pages/blog/blog1.html"),
+        blog2: resolve(__dirname, "pages/blog/blog2.html"),
+
+        cursos: resolve(__dirname, "pages/cursos.html"),
+        cursos1: resolve(__dirname, "pages/cursos/cursos1.html"),
+        cursos2: resolve(__dirname, "pages/cursos/cursos2.html"),
+
+        contacto: resolve(__dirname, "pages/contacto.html"),
+        login: resolve(__dirname, "pages/login.html"),
+        registro: resolve(__dirname, "pages/registro.html"),
+        quienesSomos: resolve(__dirname, "pages/quienes-somos.html"),
+        avisoLegal: resolve(__dirname, "pages/aviso-legal.html"),
+      }
+    }
+  },
+
   server: {
     open: true
   },
+
   css: {
     preprocessorOptions: {
       scss: {
@@ -15,12 +40,13 @@ export default defineConfig({
       }
     }
   },
+
   resolve: {
     alias: {
-      "@": "/",
-      "@sass": "/sass",
-      "@js": "/js",
-      "@img": "/img"
+      "@": resolve(__dirname, "src"),
+      "@sass": resolve(__dirname, "src/sass"),
+      "@js": resolve(__dirname, "src"),
+      "@img": resolve(__dirname, "src/assets/img")
     }
   }
 });
